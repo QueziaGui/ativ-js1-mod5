e7e519395df615194f93d7926622d070bd4e0e0a
Quezia Guilhermina Dias dos Santos <queziagds@gmail.com>
2023-10-14 22:19:31 -0300

    acertando

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let titulo = document.getElementById('titulo');
-titulo.innerText = 'Aprendendo a manipular DOM';
+const titulo = document.getElementById('titulo');
+const ul = document.querySelector('ul');
+const listaOrdenada = documento.getElementById('lista-ordenada');
+const a = document.querySelector('a')
 
-let ul = document.querySelector('ul');
-ul.innerHTML = "<ul><li> Elemento de lista JS 01</li><li> Elemento de lista JS 01</li><li> Elemento de lista JS 01</li></ul>";
+
+titulo.innerText = 'Aprendendo a manipular DOM';
+ul.innerHTML = '<li> Elemento de lista JS 01</li><li> Elemento de lista JS 01</li><li> Elemento de lista JS 01</li>';
+listaOrdenada.innerHTML =`
+<li><a href="https://prozeducacao.com.br"> link I</a></li>
+<li><a href="https://prozeducacao.com.br"> link II</a></li>
+<li><a href="https://prozeducacao.com.br"> link III</a></li>
+`;
+a.innerText = 'estude na proz educação. Professores exelentes, conteúdo de qualidade';
